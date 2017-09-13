222141fd8863b87584662ae7e8dd41d45a58d317
Zhangkun111 <2278159958@qq.com>
2017-09-13 22:58:56 +0800

    个推

---
+++ Getui/app/src/main/java/com/bwie/hp/getui/DemoIntentService.java
@@ -0,0 +1,43 @@
+package com.bwie.hp.getui;
+
+import android.content.Context;
+import android.util.Log;
+
+import com.igexin.sdk.GTIntentService;
+import com.igexin.sdk.message.GTCmdMessage;
+import com.igexin.sdk.message.GTTransmitMessage;
+
+/**
+ * 类的用途：
+ *
+ * @author HP
+ * @date 2017/9/13 22 21
+ */
+
+public class DemoIntentService extends GTIntentService {
+
+    public DemoIntentService() {
+
+    }
+
+    @Override
+    public void onReceiveServicePid(Context context, int pid) {
+    }
+
+    @Override
+    public void onReceiveMessageData(Context context, GTTransmitMessage msg) {
+    }
+
+    @Override
+    public void onReceiveClientId(Context context, String clientid) {
+        Log.e(TAG, "onReceiveClientId -> " + "clientid = " + clientid);
+    }
+
+    @Override
+    public void onReceiveOnlineState(Context context, boolean online) {
+    }
+
+    @Override
+    public void onReceiveCommandResult(Context context, GTCmdMessage cmdMessage) {
+    }
+}
